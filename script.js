f26f3a65bc96b76f87f34890133f2d42fd59f444
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-09-10 10:12:08 -0700

    add reset.css and prompts to js file

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+  var length = prompt("How many characters would you like your password to be? You can choose between 8 and 128.");
+  var numbers = prompt("Would you like your password to have numbers?");
+  var specialChar = prompt("Would you like your password to have special characters?");
+  var upper = prompt("Would you like your password to have uppercase letters?");
+  var lower = prompt("Would you like your passowrd to have loewrcase letters?");
+
   passwordText.value = password;
 
 }
