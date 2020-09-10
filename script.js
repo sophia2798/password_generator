e046b1e98152443802b0ddd1d58e27d7f813d5a2
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-09-10 14:41:45 -0700

    edited length prompt to continue to ask for a correct input if a non-accepted value is input

--- script.js
+++ script.js
@@ -20,36 +20,42 @@ function writePassword() {
         return x.toUpperCase();
      }
   
-  var upper = alpha.map(toUpper);
+  var upper = letters.map(toUpper);
+  console.log(upper)
 
+  function generatePassword() {
 // Turn the type of the lenght prompt from a string to a number
   var confLength = parseInt(prompt("How many characters would you like your password to be? You can choose between 8 and 128."));
 // Create an if statement that shows alerts if the input is less than 8 or greater than 128, another alert if it is left blank, or continues with prompts if a number between 8 and 128 is input
-  if (confLength==="") {
+  while (confLength < 8 || confLength > 128 || !confLength) {
+    if (!confLength) {
       alert("You must input a value.")
+      confLength = parseInt(prompt("How many characters would you like your password to be? You can choose between 8 and 128."));
   }
   else if (confLength < 8 || confLength > 128) {
-      alert("You must choose a number between 8 and 128.");
-      parseInt(prompt("How many characters would you like your password to be? You can choose between 8 and 128."));
+      confLength = parseInt(prompt("You did not choose a number between 8 and 128. Please choose a number between 8 and 128."));
   }
-  else {
+}
+while (confLength >= 8 && confLength <= 128) {
     var confNumbers = confirm("Would you like your password to have numbers?");
     var confSpecialChar = confirm("Would you like your password to have special characters?");
     var confUpper = confirm("Would you like your password to have uppercase letters?");
     var confLower = confirm("Would you like your passowrd to have lowercase letters?");    
   };
+}
 
   // Insert if statement if all criteria are not accepted and another if statment if all criteria are accepted
-  if (!confNumbers && !confSpecialChar && !confUpper && !confLength) {
+  if (!confNumbers && !confSpecialChar && !confUpper && !confLower) {
       alert("You must choose at least one criteria.")
   }
-  else if (confNumbers && confSpecialChar && confUpper && confLength) {
-
+  else if (confNumbers && confSpecialChar && confUpper && confLower) {
+      criteria = number.concat(specialChar,letters,upper);
   }
+
+
 
   passwordText.value = password;
   document.getElementById("password").placeholder = password;
-
 }
 
 // Add event listener to generate button
