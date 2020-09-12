04ba50c828bd0bab0e87ed3aa46e04ff0121bf17
sophia2798 <68670934+sophia2798@users.noreply.github.com>
2020-09-11 22:06:15 -0700

    add mobile responsiveness, change layout and styling of page, add additional buttons instead of prompts, and created a new javascript file script2.js to program the necessary functions for the new layout

---
+++ script2.js
@@ -0,0 +1,113 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+ 
+    // Create variables to store chosen criteria
+    let confNumbers = false;
+    let confSpecialChar = false;
+    let confUpper = false;
+    let confLower = false;
+
+    // Create variables for each switch in the HTML
+    var numSwitch = document.querySelector("#num-switch");
+    var specSwitch = document.querySelector("#special-switch");
+    var upperSwitch = document.querySelector("#upper-switch");
+    var lowerSwitch = document.querySelector("#lower-switch");
+
+   function toggle(bool) {
+       bool = !bool;
+       return bool;
+   }
+
+   numSwitch.addEventListener("click", function() {
+         confNumbers = toggle(confNumbers)
+   })
+   specSwitch.addEventListener("click", function() {
+   confSpecialChar = toggle(confSpecialChar)    
+   })
+   upperSwitch.addEventListener("click", function() {
+    confUpper = toggle(confUpper)   
+   })
+   lowerSwitch.addEventListener("click", function() {
+   confLower = toggle(confLower)    
+   })
+
+// Write password to the #password input
+function writePassword() {
+
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+function generatePassword() {
+
+    // Create a variable for the input for the password length and create conditionals to address the edge conditions
+    var length = document.querySelector("#charLength").value
+    if (isNaN(length) || !length) {
+        alert("You must enter a number!")
+    }
+    else if (parseInt(length) < 8 || parseInt(length) > 128) {
+        alert("You must enter a number between 8 and 128!")
+    }
+    else {
+        var confLength = length;
+    }
+
+    // Create arrays defining numbers, special characters, and letters
+    var number = ["0","1","2","3","4","5","6","7","8","9"];
+    var specialChar = ["!","@","#","$","%","^","&","*","(",")","-","=","_","+","<",">","/","?","`","~",":",";","'","[","]","{","}","|"];
+    var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+    // Create function to map letters to uppercase
+
+    var toUpper = function(x) {
+        return x.toUpperCase();
+    }
+    var upper = letters.map(toUpper);
+
+      // Create "if" conditions to concatenate the criteria arrays depending on the boolean output of each prompt
+  
+  // Define an empty array to store the chosen criteria
+  var criteria = [];
+ 
+  if (confNumbers) {
+      criteria = criteria.concat(number);
+  }
+  if (confSpecialChar) {
+      criteria = criteria.concat(specialChar);
+  }
+  if (confUpper) {
+      criteria = criteria.concat(upper);
+  }
+  if (confLower) {
+      criteria = criteria.concat(letters);
+  }
+  console.log(criteria)
+
+  // Create emtpy string to store each the individual characters as the "for" loop runs
+  var result = "";
+
+for (var i=0; i < confLength; i++) {
+      result = result + criteria[Math.floor(Math.random()*criteria.length)];
+  }
+  console.log(result);
+  // Have the generatePassword function return the final password string, which will not be equal to the password variable
+  return result;
+
+}
+
+  passwordText.value = password;
+  if (confLower === false && confSpecialChar === false && confUpper === false && confLower === false) {
+    passwordText.value = "You must pick at least one topping!";
+    }
+
+  // Add function to copy to clipboard
+  function copyFunc() {
+    var copy = document.getElementById("password").select();
+    document.execCommand("copy");
+    alert("Your random password has been copied to your clipboard!");
+    }
+
+    copy.addEventListener("click", copyFunc);
+
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
